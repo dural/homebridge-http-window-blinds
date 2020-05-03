a9856d00bd4473d6d6f8e7c79708b8600d9cec8b
Carlos Frutos <carlosfrutos@gmail.com>
2020-05-03 12:01:50 +0200

    First working version

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Homebridge HTTP Window Blinds
 
+Repository forked from [homebridge-http-switch](https://github.com/Supereg/homebridge-http-switch) to enable the use of regex expressions with the response from the window blind status endpoint.
+
 This is based on an Archived module by @jeffreylanters
 A simple [Homebridge](https://github.com/nfarina/homebridge) plugin for controlling window blinds over HTTP. This required a DIY device to move the windows blinds cord.
 
@@ -13,7 +15,7 @@ This plugin handles opening and closing blinds from 0% to 100%. It also updates 
 ## Installation
 
 - Install homebridge: `sudo npm install -g homebridge`
-- Install this plugin: `sudo npm install -g homebridge-http-window-blinds`
+- Install this plugin: `sudo npm install -g homebridge-http-window-blinds-pattern`
 - Update your configuration file. See config sample below.
 
 ## Config sample
@@ -24,13 +26,16 @@ This plugin handles opening and closing blinds from 0% to 100%. It also updates 
     "accessories": [
         {
             // Required
-            "accessory": "HttpWindowBlinds",
+            "accessory": "HttpWindowBlindsPattern",
             "name": "Office Blinds",
             "urlSetTargetPosition": "http://192.168.1.100/api/blinds?open=%VALUE%",
             "urlGetCurrentPosition": "http://192.168.1.100/api/status",
 
             // Optional
-            "outputValueMultiplier": 0.8
+            "statusPattern": "([0-9]+)", // This is the default pattern
+            "matchingGroup": 0,
+            "outputValueMultiplier": 0.8,  // Default value 1
+            "debug": true
         }
     ],
     // ...
@@ -41,4 +46,4 @@ This plugin handles opening and closing blinds from 0% to 100%. It also updates 
 
 When setting the target position the url from the config will be used. %VALUE% will be replaced will the value passed by your Homekit app. This will be a value between 0 and 100 depending on the status of your window covering. Once the HTTP request was done, the current position will be updated so the Homekit app will complete and can take another request. There is no time-out for this request.
 
-The responce from urlGetCurrentPosition should be json e.g. {"position":"20"} where 20 is 20% open.
+The response from urlGetCurrentPosition should match the RegExp given in statusPattern config value. This value should be an integer value within 0 and 100.
--- index.js
+++ index.js
@@ -4,87 +4,117 @@ var Characteristic;
 const request = require('request');
 
 const DEF_MIN_OPEN = 0,
-      DEF_MAX_OPEN = 100,
-      DEF_TIMEOUT = 5000;
+	DEF_MAX_OPEN = 100,
+	DEF_TIMEOUT = 5000;
 
 module.exports = homebridge => {
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
 	homebridge.registerAccessory(
-		"homebridge-http-window-blinds",
-		"HttpWindowBlinds",
-		HttpWindowBlinds);
+		"homebridge-http-window-blinds-pattern",
+		"HttpWindowBlindsPattern",
+		HttpWindowBlindsPattern);
 };
 
-function HttpWindowBlinds(log, config) {
+function extractValueFromPattern(pattern, string, position = 1) {
+	const matchArray = string.match(pattern);
+
+	if (matchArray === null) // pattern didn't match at all
+		throw new Error(`Pattern didn't match (value: '${string}', pattern: '${pattern}')`);
+	else if (position >= matchArray.length)
+		throw new Error("Couldn't find any group which can be extracted. The specified group from which the data should be extracted was out of bounds");
+	else
+		return matchArray[position];
+}
+
+function HttpWindowBlindsPattern(log, config) {
 	this.service = new Service.WindowCovering(this.name);
-    	this.log = log;
+	this.log = log;
+	this.debug = config.debug || false;
 	this.name = config.name || "Window Blinds";
 	this.model = config["model"] || "nodeMCU based DIY motorised blinds";
-	this.manufacturer = "@crashtestoz";
+	this.manufacturer = "@carlosfrutos";
 	this.outputValueMultiplier = config.outputValueMultiplier || 1;
 	this.urlSetTargetPosition = config.urlSetTargetPosition;
 	this.urlGetCurrentPosition = config.urlGetCurrentPosition;
+	this.statusPattern = "([0-9]+)";
+	if (config.statusPattern) {
+		if (typeof config.statusPattern === "string")
+			this.statusPattern = new RegExp(config.statusPattern);
+		else
+			this.log.warn("Property 'statusPattern' was given in an unsupported type. Using default one! (%s)", this.statusPattern);
+	}
+	this.matchingGroup = 1;
+	if (config.matchingGroup) {
+		if (typeof config.matchingGroup === "int")
+			this.matchingGroup = config.matchingGroup;
+		else
+			this.log.warn("Property 'matchingGroup' was given in an unsupported type. Using default one! (%s)", this.matchingGroup);
+	}
 	this.serial = config["serial"] || "HWB02";
-   	this.timeout = config["timeout"] || DEF_TIMEOUT;
-   	this.minOpen = config["min_open"] || DEF_MIN_OPEN;
-   	this.maxOpen = config["max_open"] || DEF_MAX_OPEN;
-	
+	this.timeout = config["timeout"] || DEF_TIMEOUT;
+	this.minOpen = config["min_open"] || DEF_MIN_OPEN;
+	this.maxOpen = config["max_open"] || DEF_MAX_OPEN;
+
 	this.currentPosition = 0;
 	this.targetPosition = 100;
-	
+
 	this.positionState = Characteristic.PositionState.STOPPED;
 	this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
-}	
+}
 
-HttpWindowBlinds.prototype = {
-	identify: function(callback) {
+HttpWindowBlindsPattern.prototype = {
+	identify: function (callback) {
 		this.log("Identify requested!");
 		callback(null);
 	},
-	
-	getName: function(callback) {
+
+	getName: function (callback) {
 		this.log("getName :", this.name);
 		callback(null, this.name);
 	},
 
-	getCurrentPosition: function(callback) {
+	getCurrentPosition: function (callback) {
 		var ops = {
-         		uri:    this.urlGetCurrentPosition,
-         		method: "GET",
-         		timeout: this.timeout
-      		};
+			uri: this.urlGetCurrentPosition,
+			method: "GET",
+			timeout: this.timeout
+		};
 		//GetCode here
 		request(ops, (error, response, body) => {
 			var value = null;
-         		if (error) {
-            			this.log('HTTP bad response (' + ops.uri + '): ' + error.message);
-         		} else {
-            			try {
-               				value = JSON.parse(body).position;
-               				if (value < this.minOpen || value > this.maxOpen || isNaN(value)) {
+			if (error) {
+				this.log(`HTTP bad response (${ops.uri}): ${error.message}`);
+			} else {
+				try {
+					//value = JSON.parse(body).position;
+					matches = this.statusPattern.exec(body);
+					value = matches[this.matchingGroup];
+					if (this.debug)
+						this.log(`Matched groups: ${matches}. Window blind's current position is ${matches[this.matchingGroup]}`);
+					if (value < this.minOpen || value > this.maxOpen || isNaN(value)) {
 						throw "Invalid value received";
-               				}
-               				this.log('HTTP successful response: ' + body);
+					}
+					this.log('HTTP successful response: ' + body);
 					this.currentPosition = value;
 					this.service.setCharacteristic(Characteristic.CurrentPosition, this.currentPosition);
 					this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
-					
-            			} catch (parseErr) {
-               				this.log('Error processing received information: ' + parseErr.message);
-               				error = parseErr;
-					
-            			}
-         		}
+
+				} catch (parseErr) {
+					this.log(`Error processing received information: ${parseErr.message} body: ${body}`);
+					error = parseErr;
+
+				}
+			}
 			callback(error, this.currentPosition);
-		});	
+		});
 	},
-	getTargetPosition: function(callback){
+	getTargetPosition: function (callback) {
 		this.log("getTargetPosition :", this.targetPosition);
 		this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
 		callback(null, this.targetPosition);
 	},
-	setTargetPosition: function(value, callback) {
+	setTargetPosition: function (value, callback) {
 		this.log("setTargetPosition from %s to %s", this.targetPosition, value);
 		this.targetPosition = value;
 
@@ -96,7 +126,7 @@ HttpWindowBlinds.prototype = {
 			this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
 		}
 
-		request((this.urlSetTargetPosition.replace('%VALUE%', Math.round (value * this.outputValueMultiplier))), (error, response, body) => {
+		request((this.urlSetTargetPosition.replace('%VALUE%', Math.round(value * this.outputValueMultiplier))), (error, response, body) => {
 			this.currentPosition = this.targetPosition;
 			this.service.setCharacteristic(Characteristic.CurrentPosition, this.currentPosition);
 			this.service.setCharacteristic(Characteristic.PositionState, Characteristic.PositionState.STOPPED);
@@ -104,15 +134,15 @@ HttpWindowBlinds.prototype = {
 			callback(null);
 		});
 	},
-	getPositionState: function(callback) {
+	getPositionState: function (callback) {
 		this.log("getPositionState :", this.positionState);
 		callback(null, this.positionState);
 	},
-	getServices: function() {
+	getServices: function () {
 		var informationService = new Service.AccessoryInformation();
 
 		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "Peter Chodyra")
+			.setCharacteristic(Characteristic.Manufacturer, "Peter Chodyra & Carlos Frutos")
 			.setCharacteristic(Characteristic.Model, "HTTP Window Blinds")
 			.setCharacteristic(Characteristic.SerialNumber, "HWB02");
 
